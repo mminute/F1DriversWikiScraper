9694e5832a972e52fd84b7aeded20c54f010809e
Mason Jennings <mjennings@pinterest.com>
2020-08-08 20:59:56 -0700

    change odd character identifier to [a-zA-Z\-\d]

--- utils/IdentifyOddChars.rb
+++ utils/IdentifyOddChars.rb
@@ -2,7 +2,7 @@ def identify_odd_chars(str)
     chars = str.split(//)
     odd_chars = []
     chars.each { |char|
-        if !char.match(/[a-zA-Z\-]/)
+        if !char.match(/[a-zA-Z\-\d]/)
             odd_chars.push(char)
         end
     }
